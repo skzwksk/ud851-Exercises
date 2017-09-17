e4347c15593656f6874382925e27c4bbda7a3c5c
Keisuke Sekizawa <sekizawa@team-lab.com>
2017-09-17 22:10:22 +0900

    Add one menu item to your menu and Give the menu item an id of @+id/action_search

--- Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -40,8 +40,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Do 2 - 7 in menu.xml ///////////////////////////////////////////////////////////////////////
-    // TODO (3) Add one menu item to your menu
-    // TODO (4) Give the menu item an id of @+id/action_search
     // TODO (5) Set the orderInCategory to 1
     // TODO (6) Show this item if there is room (use app:showAsAction, not android:showAsAction)
     // TODO (7) Set the title to the search string ("Search") from strings.xml
